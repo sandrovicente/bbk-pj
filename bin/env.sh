e1bc33f5a059c0fd20354cbae6b4626442a26d1c
sandrovicente <sandrovicente@gmail.com>
2014-09-08 01:45:31 +0100

    integrated pusher to active MQ

--- bin/env.sh
+++ bin/env.sh
@@ -6,12 +6,26 @@ DEST=$BASE/out
 NAMELOGS=$SOURCE/name_resolver.log
 LE_FILE=$DEST/full_le.dmp
 
-
 ELASTIC_SRV=localhost
 ELASTIC_PORT=9200
 ELASTIC_INDEX=telco
+
+MQ_SRV=localhost
+MQ_PORT=61613
+MQ_USER=admin
+MQ_PASS=admin
+
+MQ_QUEUE=HLE
 
 export COLMAT=`pwd`/../colmat
 export ANALYS=`pwd`/../analyser
 export PERL5LIB=$PERL5LIB:$COLMAT
 
+function Try {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "error with $1" >&2
+                exit $status
+    fi
+}  
--- bin/run_analysis.sh
+++ bin/run_analysis.sh
@@ -2,7 +2,6 @@
 
 source env.sh
 
-
 echo "*************************************"
 echo "* Source files in $SOURCE"
 echo "* Temporary files in $TEMP"
@@ -11,6 +10,10 @@ echo "*************************************"
 echo
 echo "* Analsys" 
 echo
+
+if [ $# -eq 1 ]; then
+    send_to_mq=true
+fi  
 
 ## should check if elasticsearch is available
 nc -z $ELASTIC_SRV $ELASTIC_PORT
@@ -36,7 +39,7 @@ popd
 
 echo "* Retrieve full LEs from analytic database"
 echo
-bash $ANALYS/query_full.sh $ELASTIC_SRV $ELASTIC_PORT $ELASTIC_INDEX $TEMP/tmp_full.csv
+Try bash $ANALYS/query_full.sh $ELASTIC_SRV $ELASTIC_PORT $ELASTIC_INDEX $TEMP/tmp_full.csv
 echo
 echo "* File generated"
 
@@ -44,6 +47,27 @@ echo
 echo "* Retrieve send data for analysis"
 echo
 pushd $ANALYS
-Rscript agg_checks.R $TEMP/tmp_full.csv $DEST/a_agg.csv
+Try Rscript agg_checks.R $TEMP/tmp_full.csv $DEST/a_agg.csv
 popd
 
+if [ $send_to_mq ]; then
+    echo "** SEND TO MQ"
+
+
+    ## should check if MQ is available
+    nc -z $MQ_SRV $MQ_PORT
+
+    if [ $status -ne 0 ]; then
+        echo "Error: ElasticSearch server is not UP"
+        echo "Please check if server is listening on host: $ELASTIC_SRV, port: $ELASTIC_PORT"
+        exit
+    fi
+
+    for F in $DEST/*.csv; do
+        
+        Try cat $F | perl $ANALYS/mq_pusher.pl $MQ_SRV $MQ_PORT $MQ_QUEUE $MQ_USER $MQ_PASS 
+
+        rm $F
+    done
+
+fi
